52d1fedd991de4ff78bd2283409f83e032558f74
Hunter Liles <hunterliles06@gmail.com>
2026-06-14 13:19:04 -0500

    Initial commit

---
+++ build/CMakeFiles/window.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/window.dir/link.d"
+  "CMakeFiles/window.dir/src/glad.c.o"
+  "CMakeFiles/window.dir/src/glad.c.o.d"
+  "CMakeFiles/window.dir/src/window.c.o"
+  "CMakeFiles/window.dir/src/window.c.o.d"
+  "window"
+  "window.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/window.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
